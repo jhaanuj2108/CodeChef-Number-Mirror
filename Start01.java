efe288e6e0390be7e34dbd19abbcf02ea8371863
Anuj Jha <jha.anuj.2108@gmail.com>
2017-10-28 01:30:36 +0530

    Create Start01.java

---
+++ Start01.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class START01 
+{
+	public static void main(String[] args) 
+	{
+		int a;
+		Scanner sc = new Scanner(System.in);
+		a =sc.nextInt();
+		System.out.println(a);
+	}
+}
